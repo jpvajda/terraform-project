67fd5ceb1c131d76a06ff7ce6f0ea0af2d0e6c13
John P Vajda <jvajda@newrelic.com>
2020-11-09 16:39:22 -0700

    configured variables

--- main.tf
+++ main.tf
@@ -1,12 +1,18 @@
 terraform {
-  # Require Terraform version 0.13.x (recommended)
   required_version = "~> 0.13.0"
 
-  # Require the latest 2.x version of the New Relic provider
   required_providers {
     newrelic = {
       source  = "newrelic/newrelic"
       version = "~> 2.12"
     }
   }
+  provider "newrelic" {
+    account_id = var.account_id
+    api_key    = var.api_key
+    region     = "US"
+  }
 }
+
+
+
